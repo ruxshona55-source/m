6ab0c64c1c5870d87d0fe1b2026add27e3ae773b
ruxshona55-source <ruxshona55@icloud.com>
2026-06-19 14:45:10 +0500

    3-normativ drf

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'accounts.authentication.CsrfExemptSessionAuthentication',
+    ],
+}
